954fa1eb0f31a40c4cbb582546feaf8fbb3b25b3
Niko <soder.n@gmail.com>
2022-11-04 12:49:32 +0200

    Remove extra carousel classes

--- src/home-page/carousel.js
+++ src/home-page/carousel.js
@@ -1,13 +1,13 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+/* let slideIndex = 1;
+showSlides(slideIndex); */
 
 // Next/previous controls
-function plusSlides(n) {
+/* function plusSlides(n) {
   showSlides((slideIndex += n));
-}
+} */
 
 // Thumbnail image controls
-function currentSlide(n) {
+/* function currentSlide(n) {
   showSlides((slideIndex = n));
 }
 
@@ -26,4 +26,22 @@ function showSlides(n) {
   }
 
   slides[slideIndex - 1].style.display = "block";
+} */
+
+/* ************************** */
+let slidePosition = 0;
+SlideShow();
+
+function SlideShow() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {
+    slidePosition = 1;
+  }
+  slides[slidePosition - 1].style.display = "block";
+  setTimeout(SlideShow, 6000); // Change image every 2 seconds
 }
